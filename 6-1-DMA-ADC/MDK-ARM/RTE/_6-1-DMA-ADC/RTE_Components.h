e49415566da93e09b0d0fc02fd941c8ba9019576
liyangchen-dev <liyangchen8888@gmail.com>
2026-03-21 21:45:08 +0800

    DMA和ADC实验

---
+++ 6-1-DMA-ADC/MDK-ARM/RTE/_6-1-DMA-ADC/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '6-1-DMA-ADC' 
+ * Target:  '6-1-DMA-ADC' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
